eeb4d18e127a02fca67c01a26f0dc12b0e450e2f
arynapril <arynapril@gmail.com>
2017-07-11 23:39:11 -0400

    fixed roll

---
+++ commands/roll.js
@@ -0,0 +1,24 @@
+exports.run = (bot, message, args) => {
+    if (args == "") {
+        var roll = Math.floor(Math.random() * 6) + 1;
+        message.reply("You rolled a **" + roll + "**!");
+    } else if (args > 9999) {
+        message.reply("Choose a lower number! :stuck_out_tongue_winking_eye: ")
+    } else {
+        var roll = Math.floor(Math.random() * args) + 1;
+        message.reply("You rolled a **" + roll + "**!");
+    }
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['diceroll', 'dice'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'roll',
+    description: 'Rolls a die of a side number up to 9999',
+    usage: 'roll <number of sides, if not included, rolls for a six sided die>'
+};
