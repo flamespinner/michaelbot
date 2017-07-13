6292703cc4c1e074f555356c62ec42c3b83f11b2
Michael Cao <mike.x.cao@gmail.com>
2017-07-13 12:34:50 -0400

    Fixed update command

--- commands/update.js
+++ commands/update.js
@@ -1,12 +1,9 @@
-//if(msg.author.id != require('./config.json').ownerID) return msg.reply("you are not allowed to do this!")
-exports.run = {
-   const child_process = require('child_process');
-
-    main: function (bot, msg) {
+const child_process = require('child_process');
+exports.run = (bot, message, args) => {
+   if(msg.author.id != require('../config.json').ownerID) return msg.reply("you are not allowed to do this!")
         msg.channel.send("Updating...").then(function (e) {
             var evaled = eval("child_process.execSync('git pull origin').toString()");
-            e.delete();
-            msg.channel.send("```" + evaled + "```").then(function (message) {
+            msg.channel.send(evaled).then(function (message) {
                 if (evaled.indexOf("Already up-to-date.") > -1) {
                     message.channel.send("There was nothing to update!");
                     return;
@@ -17,18 +14,16 @@ exports.run = {
                 }
             })
         })
-    }
 }
 
 exports.conf = {
     enabled: true,
-    guildONly: false,
+    guildOnly: false,
     permLevel: 6
 };
 
 exports.help = {
     name: 'update',
-    //type: 'owner'
     usage: 'update',
     description: 'Pulls new changes from Github and restarts.'
 };
