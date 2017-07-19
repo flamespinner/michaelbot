1aaeba9d545402ec62c01539174d43f4a6616118
Michael Wilke <mwilke16@moundsparkacademy.org>
2017-07-19 14:21:13 -0500

    Added more special stuffs

--- commands/bee.js
+++ commands/bee.js
@@ -4,6 +4,33 @@ exports.run = (bot, msg, args) => {
         msg.channel.send("According to all known laws of aviation...");
         msg.channel.send("there is no way a bee should be able to fly.");
         msg.channel.send("Its wings are too small to get its fat little body off the ground.");
+        msg.channel.send("The bee, of course, flies anyway");
+        msg.channel.send("because bees don't care what humans think is impossible.");
+        msg.channel.send("Yellow, black. Yellow, black.");
+        msg.channel.send("Yellow, black. Yellow, black.");
+        msg.channel.send("Ooh, black and yellow! Ooh, black and yellow!");
+        msg.channel.send("Barry! Breakfast is ready!");
+        msg.channel.send("Ooming!");
+        msg.channel.send("Hang on a second.");
+        msg.channel.send("Hello?");
+
+        msg.channel.send("- Barry?");
+        msg.channel.send("- Adam?");
+        msg.channel.send("- Oan you believe this is happening?");
+        msg.channel.send("- I can't. I'll pick you up.");
+        msg.channel.send("Looking sharp.");
+        msg.channel.send("Use the stairs. Your father paid good money for those.");
+        msg.channel.send("Sorry. I'm excited.");
+        msg.channel.send("Here's the graduate. We're very proud of you, son.");
+        msg.channel.send("A perfect report card, all B's.");
+        msg.channel.send("Very proud.");
+        msg.channel.send("Ma! I got a thing going here.");
+        msg.channel.send("- You got lint on your fuzz.");
+        msg.channel.send("- Ow! That's me!");
+        msg.channel.send("- Wave to us! We'll be in row 118,000.");
+        msg.channel.send("- Bye!");
+        msg.channel.send("Barry, I told you, stop flying in the house!");
+
 };
 
 exports.conf = {
@@ -16,5 +43,5 @@ exports.conf = {
 exports.help = {
     name: 'bee',
     usage: 'bee',
-    description: 'Pulls new changes from Github and restarts.'
+    description: 'just... no'
 };
