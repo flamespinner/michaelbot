ee361b1beabee95f709544dc0780e98b2aed016a
Michael Wilke <mwilke16@moundsparkacademy.org>
2017-07-29 11:44:29 -0500

    Fixed help.js

--- commands/help.js
+++ commands/help.js
@@ -1,46 +1,4 @@
 exports.run = (bot, message, params, level) => {
-    message.channel.send("hi");
-    if (!params[0]) {
-        const myCommands = bot.commands.filter(c => c.conf.permLevel <= level);
-        const commandNames = myCommands.keyArray();
-        const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-        let currentCategory = "";
-        let output = `= Command List =\n\n[Use ${bot.config.prefix}help <commandname> for details]\n`;
-        const sorted = myCommands.sort((p, c) => p.help.category > c.help.category);
-        sorted.forEach(c => {
-            if (currentCategory !== c.help.category) {
-                output += `\n== ${c.help.category}\n`;
-                currentCategory = c.help.category;
-            }
-            output += `${bot.config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-        });
-        message.channel.send(output, {
-            code: 'asciidoc'
-        });
-    }/* else {
-        let command = params[0];
-        if (bot.commands.has(command)) {
-            command = bot.commands.get(command);
-            message.channel.send(`${command.help.name}\n${command.help.description}\nusage   :: ${bot.config.prefix}${command.help.usage}\naliases :: ${command.conf.aliases.join(', ')}`, {
-                code: 'asciidoc'
-            });
-        }
-    }*/
-};
-exports.conf = {
-    enabled: true
-    , guildOnly: false
-    , aliases: ['h', 'halp']
-    , permLevel: 0
-};
-exports.help = {
-    name: 'help'
-    , category: 'Utilities'
-    , description: 'Displays all the commands avaliable for your permission level'
-    , usage: 'help <command [optional]>'
-};
-
-/*exports.run = (bot, message, params, level) => {
     const Discord = require('discord.js');
     if (!params[0]) {
         const myCommands = bot.commands.filter(c => c.conf.permLevel <= level);
@@ -80,4 +38,4 @@ exports.help = {
     name: 'Help',
     description: 'Spam',
     usage: 'Help'
-};*/
+};
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,6 @@
 module.exports = async bot => {
     await wait(1000);
-    //bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "RDY");
-    //you define the bot.log command in function.js, might as well us it
+    bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "RDY");
     bot.log("log", `I'm ready!`, "RDY");
-    bot.user.setGame("I Am GAY");
+    bot.user.setGame('laejfa;wefjawo;fj', 'http://twitch.tv/Agent_Flame', 1);
 }
