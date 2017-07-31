7e915176bb330511d9c74d7916c2dfb13bb6ea24
Michael Wilke <enarchtechnologies@gmail.com>
2017-07-30 20:13:34 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,23 +1,41 @@
 const child_process = require('child_process');
-exports.run = (bot, msg, args) => {
-    if(msg.author.id != require('../config.json').ownerID) return msg.reply("you are not allowed to do this!") 
-        msg.channel.send("Updating...").then(e => {
-            var update = child_process.execSync('git pull origin').toString()
-            e.channel.send("```" + update + "```")
-            if (update.indexOf("Already up-to-date.") > -1) {
-                e.channel.send("There was nothing to update!");
-            } else {
-                e.channel.send("New code successfully pulled!\nRestarting...")
-                process.exit();
-            }
-        })
+const Discord = require("discord.js");
+exports.run = (bot, message, args) => {
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    var updateEmbed = new Discord.RichEmbed()
+        .setColor([r, g, b])
+        .setDescription("Updating...")
+        .setTimestamp()
+        .setFooter('UPDATE')
+    message.channel.send({embed: updateEmbed}).then(e => {
+        var update = child_process.execSync('git pull origin').toString();
+        if (update.indexOf("Already up-to-date.") > -1) {
+            var updateEmbed3 = new Discord.RichEmbed()
+                .setColor([r, g, b])
+                .setTitle("Updated!")
+                .setDescription("There was nothing to update!")
+                .setTimestamp()
+                .setFooter('Code is up to date!')
+            e.edit({embed: updateEmbed3});
+        } else {
+            var updateEmbed2 = new Discord.RichEmbed()
+                .setColor([r, g, b])
+                .setTitle("Updated!")
+                .setDescription(update)
+                .setTimestamp()
+                .setFooter('New code successfully pulled! Restarting...')
+            e.edit({embed: updateEmbed2});
+        }
+    })
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['upd', 'Update'],
-    permLevel: 0
+    aliases: ['upd', 'Update', 'upd8'],
+    permLevel: 10
 };
 
 exports.help = {
